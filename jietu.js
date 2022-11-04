d6a3ef0f41d56b5a2e6222383b21bea588ceda08
siwangheiye <93850146+siwangheiye@users.noreply.github.com>
2022-11-04 11:10:23 +0800

    截图

---
+++ jietu.js
@@ -0,0 +1,9 @@
+auto(); // 自动打开无障碍服务
+setScreenMetrics(1080, 2340);
+launchApp("苏周到");sleep(5000);
+click(554,1896);
+click(694,673);sleep(100);
+click(694,673);sleep(3000);
+click(998,1058);sleep(2000);
+swipe(500,10,500,1000,2000);sleep(1000);
+click(891,941);
